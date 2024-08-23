d22b7ddd815e4ae9f3ae081df310e17607273f2e
AndreR01 <92729018+AndreR01@users.noreply.github.com>
2024-08-23 09:39:46 +0800

    Add data transformation for all independent variables
    log function for chlorides
    sqrt for sulfur dioxide & alcohol
    minmax for citric and quality

--- ASSESSMENT2/Rampono.R
+++ ASSESSMENT2/Rampono.R
@@ -12,11 +12,13 @@
 #Question 1 - Understand the Data
 ##################################
 install.packages("ggplot2")
+library(ggplot2)
 data.raw <- as.matrix(read.table("RedWine.txt"))
 
 set.seed(244906702) # using your student ID number for reproducible sampling with the seed function
 
 wines <- data.raw[sample(1:1599, 500), c(1:6)]
+wines
 
 citric <-c(wines[,1])
 chlorides <-c(wines[,2])
@@ -35,22 +37,21 @@ pairs(wines[, c(1, 3:6)], main="Scatter Plot Matrix for W Dataset")
 
 
 # Create 5 scatterplots function (for each X variable against the variable of interest Y) 
-par(mfrow=c(2,3))
 plot(citric, quality, main="Scatter plot of Citric Acid v Quatlity", xlab="Citric Acid",ylab="Quality")
 plot(chlorides,quality,main="Scatter plot of Chlorides v Quatlity", xlab="Chlorides",ylab="Quality")
 plot(sulfur_dioxide,quality,main="Scatter plot of Total Sulfur Dioxide v Quatlity", xlab="Total Sulfur Dioxide",ylab="Quality")
 plot(hydrogen,quality,main="Scatter plot of pH v Quatlity", xlab="pH",ylab="Quality")
 plot(alcohol,quality,main="Scatter plot of Alcohol v Quatlity", xlab="Alcohol",ylab="Quality")
 cor(citric,quality)
-cor.test(citric,quality)
 cor(chlorides,quality)
 cor(sulfur_dioxide,quality)
 cor(hydrogen,quality)
 cor(alcohol,quality)
+ggplot(wines, aes(x = alcohol, y = quality)) + geom_point()
 
 
 # Create 6 histograms for each X variable and Y
-par(mfrow=c(2,3))
+
 hist(citric, xlab="Citric Acid (g/L)", ylab="Frequency (wine samples)", main="Citric Acid (grams/Litre)")
 hist(chlorides, xlab="Chlorides (g/L)", ylab="Frequency (wine samples)", main="Chlorides (grams(sodium chloride)/Litre)")
 hist(sulfur_dioxide, xlab="Total Sulfur Dioxide (mg/L)", ylab="Frequency (wine samples)", main="Total Sulfur Dioxide (milligrams/Litre)")
@@ -95,6 +96,8 @@ quality.mean <-mean(quality)
 quality.mean
 quality.sd <-sqrt(sum((quality.mean)^2)/(length(quality)-1))
 quality.sd
+quality.median <-median(quality)
+quality.median
 
 #KS test for all variables
 ks.test(citric,"pnorm",mean=citric.mean,sd=citric.sd)
@@ -106,18 +109,155 @@ ks.test(quality,"pnorm",mean=quality.mean,sd=quality.sd)
 
 
 
-I <- c("define your variable index") # Choose any four X variables and Y
+I <- c(chlorides) # Choose any four X variables and Y
+
+wines.sample <- wines[sample(1:500, 400), c(1,3,4,5:6)]
+wines.sample
+
 
 variables_for_transform <- data.subset[,I]  # obtain a 400 by 5 matrix
 
 # for each variable, you need to figure out a good data transformation method, 
 # such as Polynomial, log and negation transformation. The k-S test and Skewness 
 # calculation may be helpful to select the transformation method
+s_citric <-c(wines.sample[,1])
+s_chlorides <-c(wines.sample[,2])
+s_sulfur_dioxide <-c(wines.sample[,3])
+#s_hydrogen <-c(wines.sample[,3])
+s_alcohol <-c(wines.sample[,4])
+s_quality <-c(wines.sample[,5])
 
- 
+#mean and modes
+s_citric_mean <-mean(s_citric)
+s_chlorides_mean <-mean(s_chlorides)
+s_sulfur_dioxide_mean <-mean(s_sulfur_dioxide)
+s_alcohol_mean <-mean(s_alcohol)
+s_quality_mean <-mean(s_quality)
+
+s_citric_median <-median(s_citric)
+s_chlorides_median <-median(s_chlorides)
+s_sulfur_dioxide_median <-median(s_sulfur_dioxide)
+s_alcohol_median <-median(s_alcohol)
+s_quality_median <-median(s_quality)
+
+s_citric_mean
+s_citric_median
+
+s_chlorides_mean
+s_chlorides_median
+
+s_sulfur_dioxide_mean
+s_sulfur_dioxide_median
+
+s_alcohol_mean
+s_alcohol_median
+
+s_quality_mean
+s_quality_median
+
+#Test log funtion
+hist(chlorides, xlab="Chlorides (g/L)", ylab="Frequency (wine samples)", main="Chlorides (grams(sodium chloride)/Litre)")
+hist(log(s_chlorides))
+min_max_l_chlorides <-minmax(log(s_chlorides))
+hist(min_max_l_chlorides)
+hist(s_chlorides^3)
+hist(sulfur_dioxide, xlab="Total Sulfur Dioxide (mg/L)", ylab="Frequency (wine samples)", main="Total Sulfur Dioxide (milligrams/Litre)")
+hist(hydrogen, xlab="pH", ylab="Frequency (wine samples)", main="pH")
+hist(alcohol, xlab="Alcohol %", ylab="Frequency (wine samples)", main="Alcohol (volume as a %")
+hist(quality, xlab="Quality", ylab="Frequency (wine samples)", main="Quatlity")
+
+hist(s_alcohol^2)
+
+
+#Transfrom citric acid by z-score standardisation and scaling to unit interval
+#unit.z <- function(x){
+#  0.15*((x-mean(x))/sd(x)) + 0.5
+#}
+#citric.z <-unit.z(s_citric)
+#s_citric
+#citric.z
+#plot(citric.z, s_quality)
+#plot(s_citric, s_quality)
+
+#hist(citric.z)
+#hist(s_citric)
+#s_citric_m <-mean(s_citric)
+#s_citric_sd <-sd(s_citric)
+#ks.test(s_citric,"pnorm",mean=s_citric_m, sd=s_citric_sd)
+
+#citric.z_m <-mean(citric.z)
+#citric.z_sd <-sd(citric.z)
+#ks.test(citric.z,"pnorm",mean=citric.z_m, sd=citric.z_sd)
+
+#qqnorm(s_citric, main='Non-normal')
+#qqline(s_citric)
+
+#qqnorm(citric.z, main='Non-normal')
+#qqline(citric.z)
+
+#unit.z <- function(x){
+#  0.15*((x-mean(x))/sd(x)) + 0.5
+#}
+
+
+#Use log transformation for chlorides due to strong postive skew
+min_log <-log(min(wines.sample[,2]))
+max_log <-log(max(wines.sample[,2]))
+log_chlorides <- (1-((log(wines.sample[,2])-min_log)/(max_log-min_log)))
+log_chlorides
+plot(log_chlorides,s_quality,main="Scatter plot of Chlorides v Quatlity", xlab="Chlorides",ylab="Quality")
+hist(log_chlorides, xlab="Alcohol %", ylab="Frequency (wine samples)", main="Alcohol (volume as a %")
+
+
+#Use square root transformation for sulfur dioxide as not as skewed as chlorides
+min_sqrt_sulfur_dioxide <-sqrt(min(wines.sample[,3]))
+max_sqrt_sulfur_dioxide <-sqrt(max(wines.sample[,3]))
+sqrt_sulfur_dioxide <- (1-((sqrt(wines.sample[,3])-min_sqrt_sulfur_dioxide)/(max_sqrt_sulfur_dioxide-min_sqrt_sulfur_dioxide)))
+sqrt_sulfur_dioxide
+plot(sqrt_sulfur_dioxide,s_quality,main="Scatter plot of Total Sulfur Dioxide (milligrams/Litre) v Quatlity", xlab="Total Sulfur Dioxide", ylab="Quality")
+hist(sqrt_sulfur_dioxide, xlab="Total Sulfur Dioxide", ylab="Frequency (wine samples)", main="Total Sulfur Dioxide (milligrams/Litre)")
+
+
+
+#Use square root transformation for alcohol as not as skewed as chlorides
+#min_sqrt_hydrogen <-sqrt(min(wines.sample[,4]))
+#max_sqrt_hydrogen <-sqrt(max(wines.sample[,4]))
+#sqrt_hydrogen <- (1-((sqrt(wines.sample[,4])-min_sqrt_hydrogen)/(max_sqrt_hydrogen-min_sqrt_hydrogen)))
+#sqrt_hydrogen
+#plot(sqrt_hydrogen,s_quality,main="Scatter plot of pH v Quatlity", xlab="pH",ylab="Quality")
+#hist(sqrt_hydrogen, xlab="Alcohol %", ylab="Frequency (wine samples)", main="Alcohol (volume as a %")
+#sqrt_hydrogen.mean <-mean(sqrt_hydrogen)
+#sqrt_hydrogen.mean
+#sqrt_hydrogen.sd <-sqrt(sum((sqrt_hydrogen.mean)^2)/(length(sqrt_hydrogen)-1))
+#sqrt_alcohol.sd
+#ks.test(sqrt_hydrogen,"pnorm",mean=sqrt_hydrogen.mean,sd=sqrt_alcohol.sd)
+
+
+#Normalise citric with minmax
+
+minmax <- function(x){
+  (x - min(x))/(max(x)-min(x))
+}
+min_max_citric <-minmax(s_citric)
+plot(min_max_citric,s_quality,main="Scatter plot of minmax Citric v Quality", xlab="minmax Citric",ylab="s_quality")
+plot(s_citric)
+hist(min_max_citric)
+hist(s_citric)
+
+
+#Normalise alcohol with log transformation and standardise min/max
+log_s_alcohol <-log(s_alcohol)
+min_max_log_s_alcohol <-minmax(log_s_alcohol)
+plot(min_max_log_s_alcohol,s_quality,main="Scatter plot of Alcohol (log and min-max transformation) v Quality", xlab="Alc - ln+min_max",ylab="s_quality")
+plot(s_alcohol,s_quality)
+hist(log_s_alcohol)
+hist(s_alcohol)
 
 p=0.5 # for example, using p=0.5 to transform the first variable. You should change p based on your distribution.
-data.transformed[,1]=variables_for_transform[,1]^p 
+wines.sample[,1]=wines.sample[,1]^p
+wines.sample
+s_chlorides <- c(wines.sample[,1])
+plot(s_chlorides,s_qaulity,main="Scatter plot of Chlorides v Quatlity", xlab="Chlorides",ylab="Quality")
 
 # A Min-Max and/or Z-score transformation should then be used to adjust the scale of each variable
 
@@ -127,11 +267,39 @@ minmax <- function(x){
 }
 
 # z-score standardisation and scaling to unit interval
-unit.z <- function(x){
-  0.15*((x-mean(x))/sd(x)) + 0.5
-}
+#unit.z <- function(x){
+#  0.15*((x-mean(x))/sd(x)) + 0.5
+#}
 
 data.transformed[,1]=minmax(data.transformed[,1]) # for example,using min-max normalisation for the first varible.
+
+#Check correlation of data
+cor(min_max_citric,s_quality)
+cor(log_chlorides,s_quality)
+cor(sqrt_sulfur_dioxide,s_quality)
+#cor(hydrogen,s_quality)
+cor(log_s_alcohol,s_quality)
+
+#Alcohol scatterplot overlay
+# define sample data frames 
+sample_data <- data.frame(x=c(1, 2, 3, 4, 5), 
+                          y1 = c(7, 10, 26, 39, 5), 
+                          y2 = c(4, 14, 16, 29, 15), 
+                          y3 = c(2, 13, 36, 19, 25), 
+                          y4 = c(8, 11, 6, 9, 35)) 
+
+
+# create base scatter plot 
+plot(sample_data$x, sample_data$y1) 
+
+# overlay scatter plot  
+points(sample_data$x, sample_data$y2, col='green', pch=12) 
+points(sample_data$x, sample_data$y3, col='red', pch=13) 
+
+#Rank quality
+min_max_quality <-minmax(s_quality)
+min_max_quality
+hist(min_max_quality)
 
 
 # Save this transformed data to a text file
